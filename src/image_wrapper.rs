4b264a046ec87124f563bf18dfaf7fcddf3510d8
Michael Baxter <meb.michael@gmail.com>
2018-12-01 16:40:36 -0800

    Add image wrapper and output test image

---
+++ src/image_wrapper.rs
@@ -0,0 +1,41 @@
+extern crate image;
+
+use self::image::ColorType;
+use self::image::png::PNGEncoder;
+use std::fs::File;
+
+pub struct ImageDataRGB {
+    pub width: u32,
+    pub height: u32,
+    pub pixels: Vec<u8>
+}
+
+impl ImageDataRGB {
+    pub fn new(width: u32, height: u32) -> Self {
+        let pixels_len = (width * height * 3) as usize;
+        ImageDataRGB{
+            width, height,
+            pixels: vec!{0; pixels_len}
+        }
+    }
+
+    pub fn set_pixel(&mut self, pixel: (u32, u32), rgb: (u8, u8, u8)) {
+        if pixel.0 > self.width || pixel.1 > self.height {
+            return;
+        }
+
+        let offset = (pixel.0 + pixel.1 * self.width) as usize * 3;
+        self.pixels[offset] = rgb.0;
+        self.pixels[offset + 1] = rgb.1;
+        self.pixels[offset + 2] = rgb.2;
+    }
+
+    pub fn save(&self, filename: &str) -> Result<(), std::io::Error> {
+        let file = File::create(filename)?;
+
+        let encoder = PNGEncoder::new(file);
+        encoder.encode(&self.pixels, self.width, self.height, ColorType::RGB(8))?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod image_wrapper;
+
+use image_wrapper::ImageDataRGB;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut image = ImageDataRGB::new(200, 100);
+
+    for j in 0..image.height {
+        for i in 0..image.width {
+            
+            let r = (i as f32) / (image.width as f32);
+            let g = ((image.height - 1 - j) as f32) / (image.height as f32);
+            let b: f32 = 0.2;
+
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+
+            image.set_pixel((i, j), (ir, ig, ib));
+        }
+    }
+
+    let image_name = "output/image.png";
+    let result = image.save(image_name);
+    match result {
+        Ok(()) => println!("Image saved to {}", image_name),
+        Err(err) => println!("{:?}", err)
+    }
 }
