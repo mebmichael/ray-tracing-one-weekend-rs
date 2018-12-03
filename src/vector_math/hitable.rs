236306ab245131df26bf092531a4ae72b7c322ac
Michael Baxter <meb.michael@gmail.com>
2018-12-02 16:33:57 -0800

    Add HitableList

--- src/vector_math/hitable.rs
+++ src/vector_math/hitable.rs
@@ -1,10 +1,25 @@
 use vector_math::ray::Ray;
 use vector_math::vec3::Vec3;
 
+#[derive(Copy, Clone)]
 pub struct HitRecord {
     pub t: f32,
     pub p: Vec3,
     pub normal: Vec3,
+}
+
+impl HitRecord {
+    pub fn zero() -> Self {
+        HitRecord {
+            t: 0.0,
+            p: Vec3::zero(),
+            normal: Vec3::zero(),
+        }
+    }
+
+    pub fn new(t: f32, p: Vec3, normal: Vec3) -> Self {
+        HitRecord { t, p, normal }
+    }
 }
 
 pub trait Hitable {
---
+++ src/vector_math/hitable_list.rs
@@ -0,0 +1,25 @@
+use vector_math::hitable::{HitRecord, Hitable};
+use vector_math::ray::Ray;
+
+pub struct HitableList {
+    pub list: Vec<Box<Hitable>>
+}
+
+impl Hitable for HitableList {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, record: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::zero();
+
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for item in &self.list {
+            if item.hit(ray, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *record = temp_rec;
+            }
+        }
+
+        hit_anything
+    }
+}
