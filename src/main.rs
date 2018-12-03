dfe11c685d59ab026dab244f30cb85e40471cc4d
Michael Baxter <meb.michael@gmail.com>
2018-12-02 16:34:35 -0800

    Add world list
    Finish chapter 5

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
 mod image_wrapper;
 mod vector_math;
 
-pub use image_wrapper::ImageDataRGB;
-pub use vector_math::vec3::Vec3;
-pub use vector_math::ray::Ray;
+pub use image_wrapper::*;
+pub use vector_math::hitable::*;
+pub use vector_math::hitable_list::*;
+pub use vector_math::ray::*;
+pub use vector_math::sphere::*;
+pub use vector_math::vec3::*;
 
-fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
-    let oc = ray.origin - *center; // vector from sphere center to ray origin
+fn color(r: &Ray, world: &HitableList) -> Vec3 {
+    let mut rec = HitRecord::zero();
 
-    let a = ray.direction.dot(ray.direction);
-    let b = 2.0 * oc.dot(ray.direction);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-b - f32::sqrt(discriminant)) / (2.0 * a)
-    }
-}
-
-fn color(r: &Ray) -> Vec3 {
-    let sphere_center = Vec3::new(0.0, 0.0, -1.0);
-    let t = hit_sphere(&sphere_center, 0.5, r);
-    if t > 0.0 {
-        let n = (r.point_at(t) - sphere_center).normalized();
-        (n + Vec3::new(1.0, 1.0, 1.0)) * 0.5
+    if world.hit(r, 0.0, std::f32::MAX, &mut rec) {
+        (rec.normal + Vec3::new(1.0, 1.0, 1.0)) * 0.5
     } else {
         let unit_direction = r.direction.normalized();
         let t = 0.5 * (unit_direction.y + 1.0);
@@ -44,14 +31,20 @@ fn main() {
     let image_width = image.width as f32;
     let image_height = image.height as f32;
 
+    let sphere1 = Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5));
+    let sphere2 = Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0));
+
+    let world = HitableList {
+        list: vec![sphere1, sphere2],
+    };
+
     for j in 0..image.height {
         for i in 0..image.width {
-
             let u = i as f32 / image_width;
             let v = j as f32 / image_height;
 
             let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
-            let c = color(&r);
+            let c = color(&r, &world);
 
             let r = (255.99 * c.x) as u8;
             let g = (255.99 * c.y) as u8;
