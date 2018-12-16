1428da312bcc66e7309984b82a3dbba0f1c0f54e
Michael Baxter <meb.michael@gmail.com>
2018-12-15 18:17:35 -0800

    Add random_in_unit_disk()

--- src/vector_math/random_methods.rs
+++ src/vector_math/random_methods.rs
@@ -11,3 +11,14 @@ pub fn random_in_unit_sphere() -> Vec3 {
         }
     }
 }
+
+pub fn random_in_unit_disk() -> Vec3 {
+    let mut rng = thread_rng();
+    loop {
+        let p = 2.0 * Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), 0.0) - Vec3::new(1.0, 1.0, 0.0);
+
+        if p.squared_magnitude() < 1.0 {
+            return p;
+        }
+    }
+}
